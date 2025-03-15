717fe5a2901f0dfc36a438a61a5e13d6d12e2ed3
BogdanM <bm659@cairn.edu>
2025-03-15 09:36:34 -0400

    update to character.java

--- src/Character.java
+++ src/Character.java
@@ -1,5 +1,8 @@
 import java.util.Random;
 
 public class Character {
-
+    protected String name;
+    protected double health;
+    protected double attackPower;
+    protected boolean isAlive;
 }
