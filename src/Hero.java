19e582cb641bcaa3cdd01df315fa31057f57677f
Tykhyk <tykhykzolotarov@gmail.com>
2025-03-18 10:48:03 -0400

    I added all required functions for item and most of them for hero

--- src/Hero.java
+++ src/Hero.java
@@ -1,5 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Hero extends Character {
+    private int level;
+    private int experience;
+    private float money;
+    List<Item> inventory;
 
+    public Hero (float health, float attackPower, boolean runAway){
+        super("Spiderman", health, attackPower, runAway);
+        this.level = 1;
+        this.experience = 0;
+        this.money = 0.0F;
+        this.inventory = new ArrayList<>();
+        this.inventory.add(new Item("Potion", 20));
+    }
+
+    // all function(s) for this class
+    public boolean fight(Enemy enemy){
+
+    }
+
+    public void useItem(int){
+        if (!inventory.isEmpty()){
+            health += Item.getHealingPower();
+            Item item = inventory.remove(0);
+            System.out.println("A potion healed " + super.name + " by " + Item.getHealingPower() + "HP.");
+        } else {
+            System.out.println("Inventory is empty!");
+        }
+    }
+
+    public void levelUp(){
+        if(experience >= 100){
+            level++;
+            experience -= 100;
+            health += health * .1;
+            attackPower += attackPower * .1;
+            System.out.println("Hero just leveled up, its health and attack power were increased by 10% and remaining experience is " + experience + ".");
+        }
+    }
+
+    public boolean addToInventory(Item item){
+        return (inventory.size() < 5) ? inventory.add(item) : false;
+    }
+
+    public String showInventory(){
+        StringBuilder inventoryList = new StringBuilder("Inventory: ");
+        for (Item item : inventory){
+            inventoryList.append("\n").append(item.toString());
+        }
+        return inventoryList.toString();
+    }
+
+    public String info(){
+        return "This is " + super.name + ". Health: " + super.health + ", level: " + level + ", experience: " + experience + ", money: " + money + ", attack power: " + super.attackPower + ".";
+    }
+
+
+
+    //getters and setters
+    public int getLevel(){
+        return level;
+    }
+
+    public void setLevel(int level){
+        this.level = level;
+    }
+
+    public int getExperience(){
+        return experience;
+    }
+
+    public void setExperience(int experience){
+        this.experience = experience;
+    }
+
+    public float getMoney(){
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
 }
---
+++ src/Item.java
@@ -0,0 +1,36 @@
+<<<<<<< Updated upstream
+=======
+public class Item {
+    private String name;
+    private static int healingPower;
+
+    public Item (String name, int healingPower){
+        this.name = name;
+        this.healingPower = healingPower;
+    }
+
+    // all function(s) for this class
+    public String info(){
+        return "This is " + name + " and its healing power is " + healingPower;
+    }
+
+
+
+    //Getters and setters
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public static int getHealingPower(){
+        return healingPower;
+    }
+
+    public void setHealingPower( int healingPower){
+        this.healingPower = healingPower;
+    }
+}
+>>>>>>> Stashed changes
