a4775b4983f05377a8ff84d3559964fa606ed4b3
Tykhyk <tykhykzolotarov@gmail.com>
2025-03-18 16:01:42 -0400

    All Junit tests for my part are done

--- src/Hero.java
+++ src/Hero.java
@@ -1,108 +1,107 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Hero extends Character {
-    private static int level;
-    private static int experience;
-    private static float money;
-    List<Item> inventory;
+    private int level;
+    private int experience;
+    private float money;
+    private Item[] inventory;
 
-    public Hero (float health, float attackPower, boolean runAway){
-        super("Spiderman", health, attackPower, runAway);
+    public Hero(float health, float attackPower, boolean isAlive) {
+        super("Spiderman", health, attackPower, isAlive);
         this.level = 1;
         this.experience = 0;
-        this.money = 0.0F;
-        this.inventory = new ArrayList<>();
-        this.inventory.add(new Item("Potion", 20));
+        this.money = 0.0f;
+        this.inventory = new Item[5];
+        this.inventory[0] = new Item("Potion", 20);
     }
 
-public boolean fight(Enemy enemy) {
-    Random random = new Random();
-    if (random.nextDouble() < 0.1) { // 10% chance of missing the attack
-        System.out.println("Attack is missed");
+    public boolean fight(Enemy enemy) {
+        Random rand = new Random();
+        boolean useSpecial = rand.nextBoolean();
+        boolean hit = rand.nextInt(100) > 10;
+
+        if (!hit) {
+            System.out.println(getName() + "Attack is missed!");
+            return false;
+        }
+
+        float damage = getAttackPower() * (useSpecial ? 2 : 1);
+        System.out.println(getName() + (useSpecial ? " used a special attack!" : " attacked") +
+                " and dealt " + damage + " damage points to " + enemy.getName());
+        return true;
+    }
+
+    public int useItem(int index) {
+        if (index < inventory.length && inventory[index] != null) {
+            int healing = inventory[index].getHealingPower();
+            setHealth(getHealth() + healing);
+            System.out.println(inventory[index].getName() + " healed " + getName() + " by " + healing + " HP.");
+            inventory[index] = null;
+            return healing;
+        }
+        return 0;
+    }
+
+    public boolean addToInventory(Item item) {
+        for (int i = 0; i < inventory.length; i++) {
+            if (inventory[i] == null) {
+                inventory[i] = item;
+                return true;
+            }
+        }
         return false;
     }
 
-    boolean specialAttack = random.nextBoolean();
-    double damage;
-
-    if (specialAttack) {
-        damage = getAttackPower() * 2;
-        System.out.println(getName() + " attacked enemy with special attack by " + damage + " HP points.");
-    } else {
-        damage = getAttackPower();
-        System.out.println(getName() + " attacked enemy by " + damage + " HP points.");
-    }
-
-    enemy.takeDamage(damage);
-    return true;
-}
-
-
-
-public void useItem() {
-    if (!inventory.isEmpty()) {
-        Item item = inventory.remove(0);
-        setHealth(getHealth() + item.getHealingPower());
-        System.out.println("A potion healed " + getName() + " by " + item.getHealingPower() + " HP.");
-    } else {
-        System.out.println("Inventory is empty!");
-    }
-}
-
-
-
-    public void levelUp(){
-        if(experience >= 100){
+    public void levelUp() {
+        if (experience > 100) {
             level++;
-            experience -= 100;
-            health += health * .1;
-            attackPower += attackPower * .1;
-            System.out.println("Hero just leveled up, its health and attack power were increased by 10% and remaining experience is " + experience + ".");
+            experience = 0;
+            setHealth(getHealth() * 1.1f);
+            setAttackPower(getAttackPower() * 1.1f);
+            System.out.println(getName() + " is level  " + level + "!");
         }
     }
 
-    public boolean addToInventory(Item item){
-        return (inventory.size() < 5) ? inventory.add(item) : false;
-    }
-
-    public String showInventory(){
-        StringBuilder inventoryList = new StringBuilder("Inventory: ");
-        for (Item item : inventory){
-            inventoryList.append("\n").append(item.toString());
+    public String showInventory() {
+        StringBuilder sb = new StringBuilder();
+        for (Item item : inventory) {
+            if (item != null) sb.append(item.info()).append("\n");
         }
-        return inventoryList.toString();
+        return sb.toString();
     }
 
-    public static String info(){
-        return "This is " + name + ". Health: " + health + ", level: " + level + ", experience: " + experience + ", money: " + money + ", attack power: " + attackPower + ".";
+    @Override
+    public String info() {
+        return super.info() + ", Level: " + level + ", Experience: " + experience + ", Money: " + money;
     }
 
 
 
-    //getters and setters
-    public int getLevel(){
+    public int getLevel() {
         return level;
     }
-
-    public void setLevel(int level){
+    public void setLevel(int level) {
         this.level = level;
     }
 
-    public int getExperience(){
+    public int getExperience() {
         return experience;
     }
-
-    public void setExperience(int experience){
+    public void setExperience(int experience) {
         this.experience = experience;
     }
 
-    public float getMoney(){
+    public float getMoney() {
         return money;
     }
-
     public void setMoney(float money) {
         this.money = money;
+    }
+
+    public Item[] getInventory() {
+        return inventory;
+    }
+    public void setInventory(Item[] inventory) {
+        this.inventory = inventory;
     }
 }
--- src/Item.java
+++ src/Item.java
@@ -1,34 +1,30 @@
 public class Item {
-private String name;
-private int healingPower;
+    private String name;
+    private int healingPower;
 
-
-    public Item (String name, int healingPower){
+    public Item(String name, int healingPower) {
         this.name = name;
         this.healingPower = healingPower;
     }
 
-    // all function(s) for this class
-    public static String info(){
-        return "This is " + name + " and its healing power is " + healingPower;
-    }
 
-    
-
-    //Getters and setters
     public String getName() {
-    return name;
-}
+        return name;
+    }
 
-    public void setName(String name){
+    public int getHealingPower() {
+        return healingPower;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-public int getHealingPower() {
-    return healingPower;
-}
-
-    public void setHealingPower( int healingPower){
+    public void setHealingPower(int healingPower) {
         this.healingPower = healingPower;
+    }
+
+    public String info() {
+        return "Item: " + name + ", Heals: " + healingPower;
     }
 }
